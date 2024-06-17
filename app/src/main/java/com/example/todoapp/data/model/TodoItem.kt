f7a3251c400d6cddd0584503072d29b74fa5ae44
sema <buglaev.sa@edu.spbstu.ru>
2024-06-17 10:47:04 +0300

    added model class - TodoItem

---
+++ app/src/main/java/com/example/todoapp/data/model/TodoItem.kt
@@ -0,0 +1,11 @@
+package com.example.todoapp.data.model
+
+data class TodoItem(
+    val id: String,
+    val text: String,
+    val importance: Importance,
+    var deadline: Long? = null,
+    var isDone: Boolean = false,
+    val creationDate: Long,
+    var modificationDate: Long? = null
+)
---
+++ app/src/main/java/com/example/todoapp/utils/Constants.kt
@@ -0,0 +1,1 @@
+package com.example.todoapp.utils
