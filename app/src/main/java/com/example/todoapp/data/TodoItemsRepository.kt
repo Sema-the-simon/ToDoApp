301562222faf1bff3898932e3ba559b19212ca89
sema <buglaev.sa@edu.spbstu.ru>
2024-06-17 11:31:21 +0300

    added hardcoded data

--- app/src/main/java/com/example/todoapp/data/TodoItemsRepository.kt
+++ app/src/main/java/com/example/todoapp/data/TodoItemsRepository.kt
@@ -1,13 +1,14 @@
 package com.example.todoapp.data
 
 import com.example.todoapp.data.model.TodoItem
+import com.example.todoapp.utils.getData
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class TodoItemsRepository() : Repository {
-    private val todoItems: MutableList<TodoItem> = mutableListOf()
+    private val todoItems: MutableList<TodoItem> = getData()
     private val todoItemsFlow: MutableStateFlow<List<TodoItem>> = MutableStateFlow(mutableListOf())
 
     override fun getTodoItems(): StateFlow<List<TodoItem>> = todoItemsFlow.asStateFlow()
---
+++ app/src/main/java/com/example/todoapp/utils/TestData.kt
@@ -0,0 +1,93 @@
+package com.example.todoapp.utils
+
+import android.icu.util.Calendar
+import com.example.todoapp.data.model.Importance
+import com.example.todoapp.data.model.TodoItem
+import kotlin.random.Random
+
+fun getData(): MutableList<TodoItem> {
+    val data = mutableListOf<TodoItem>()
+    val texts = listOf(
+        "Купить что-то",
+        "Купить что-то где-то, но зачем не очень понятно",
+        "Купить что-то где-то, но зачем не очень понятно, но точно чтобы показать как обрезается"
+    )
+
+    data.add(
+        TodoItem(
+            id = 0.toString(),
+            text = texts[0],
+            importance = Importance.BASIC,
+            isDone = true,
+            creationDate = Calendar.getInstance().also { it.set(Calendar.DAY_OF_WEEK, -1) }.timeInMillis
+        )
+    )
+
+    data.add(
+        TodoItem(
+            id = 1.toString(),
+            text = texts[0],
+            importance = Importance.BASIC,
+            creationDate = Calendar.getInstance().also { it.set(Calendar.DAY_OF_WEEK, -2) }.timeInMillis
+        )
+    )
+
+    data.add(
+        TodoItem(
+            id = 2.toString(),
+            text = texts[1],
+            importance = Importance.BASIC,
+            creationDate = Calendar.getInstance().also { it.set(Calendar.DAY_OF_WEEK, -3) }.timeInMillis
+        )
+    )
+
+    data.add(
+        TodoItem(
+            id = 3.toString(),
+            text = texts[2],
+            importance = Importance.BASIC,
+            creationDate = Calendar.getInstance().also { it.set(Calendar.DAY_OF_WEEK, -4) }.timeInMillis
+        )
+    )
+
+    data.add(
+        TodoItem(
+            id = 4.toString(),
+            text = texts[0],
+            importance = Importance.IMPORTANT,
+            creationDate = Calendar.getInstance().timeInMillis
+        )
+    )
+
+    data.add(
+        TodoItem(
+            id = 5.toString(),
+            text = texts[0],
+            importance = Importance.LOW,
+            creationDate = Calendar.getInstance().timeInMillis
+        )
+    )
+
+    data.add(
+        TodoItem(
+            id = 6.toString(),
+            text = texts[0],
+            importance = Importance.BASIC,
+            deadline = Calendar.getInstance().also { it.add(Calendar.DAY_OF_MONTH, 2) }.timeInMillis,
+            creationDate = Calendar.getInstance().timeInMillis
+        )
+    )
+
+    for (i in 7..15) {
+        data.add(
+            TodoItem(
+                id = i.toString(),
+                text = texts.random(),
+                importance = Importance.entries.random(),
+                creationDate = Calendar.getInstance().timeInMillis,
+                isDone = Random.nextBoolean()
+            )
+        )
+    }
+    return data
+}
