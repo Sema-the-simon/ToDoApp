ba3b46e76a950cce64081df39c5b79b180614624
sema <buglaev.sa@edu.spbstu.ru>
2024-06-26 16:53:22 +0300

    add priority symbols

--- app/src/main/java/com/example/todoapp/ui/screens/list/components/TodoListItem.kt
+++ app/src/main/java/com/example/todoapp/ui/screens/list/components/TodoListItem.kt
@@ -18,16 +18,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.todoapp.R
 import com.example.todoapp.data.model.Importance
+import com.example.todoapp.data.model.Importance.BASIC
+import com.example.todoapp.data.model.Importance.IMPORTANT
+import com.example.todoapp.data.model.Importance.LOW
 import com.example.todoapp.data.model.TodoItem
 import com.example.todoapp.ui.themes.LightBackSecondary
 import com.example.todoapp.ui.themes.LightLabelPrimary
+import com.example.todoapp.ui.themes.LightLabelSecondary
 import com.example.todoapp.ui.themes.LightLabelTertiary
 import com.example.todoapp.ui.themes.LightSupportSeparator
 import com.example.todoapp.utils.formatLongToDatePattern
@@ -48,11 +54,11 @@ fun TodoListItem(
             checked = todoItem.isDone,
             onCheckedChange = { onCheckboxClick() },
             colors = CheckboxDefaults.colors(
-                uncheckedColor = if (todoItem.importance == Importance.IMPORTANT) Color.Red
+                uncheckedColor = if (todoItem.importance == IMPORTANT) Color.Red
                 else LightSupportSeparator,
                 checkedColor = Color.Green
             ),
-            modifier = if (todoItem.importance == Importance.IMPORTANT)
+            modifier = if (todoItem.importance == IMPORTANT)
                 Modifier
                     .padding(start = 4.dp)
                     .drawBehind {
@@ -66,27 +72,36 @@ fun TodoListItem(
                     } else Modifier.padding(start = 4.dp)
         )
         Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .clickable { onItemClick() }
                 .padding(vertical = 12.dp)
 
         ) {
+
             Column(
                 modifier = Modifier
                     .weight(1f)
                     .padding(end = 12.dp)
             ) {
-                Text(
-                    text = todoItem.text,
-                    color = if (todoItem.isDone) LightLabelTertiary
-                    else LightLabelPrimary,
-                    maxLines = 3,
-                    overflow = TextOverflow.Ellipsis,
-                    style = TextStyle(
-                        textDecoration = if (todoItem.isDone) TextDecoration.LineThrough
-                        else TextDecoration.None
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    if (!todoItem.isDone) {
+                        ImportanceSymbol(todoItem.importance)
+                    }
+                    Text(
+                        text = todoItem.text,
+                        color = if (todoItem.isDone) LightLabelTertiary
+                        else LightLabelPrimary,
+                        maxLines = 3,
+                        overflow = TextOverflow.Ellipsis,
+                        style = TextStyle(
+                            textDecoration = if (todoItem.isDone) TextDecoration.LineThrough
+                            else TextDecoration.None
+                        )
                     )
-                )
+                }
                 if (todoItem.deadline != null)
                     Text(
                         text = formatLongToDatePattern(todoItem.deadline),
@@ -105,6 +120,31 @@ fun TodoListItem(
     }
 }
 
+@Composable
+fun ImportanceSymbol(importance: Importance) {
+    when (importance) {
+        LOW -> {
+            Icon(
+                painter = painterResource(id = R.drawable.low_priority_arrow),
+                contentDescription = null,
+                tint = LightLabelSecondary,
+                modifier = Modifier.padding(horizontal = 2.dp)
+            )
+        }
+
+        IMPORTANT -> {
+            Text(
+                text = "!!",
+                fontSize = 24.sp,
+                color = Color.Red,
+                modifier = Modifier.padding(horizontal = 6.dp)
+            )
+        }
+
+        else -> {}
+    }
+}
+
 @Preview(widthDp = 360, heightDp = 720)
 @Composable
 private fun TodoItemPreview() {
@@ -117,7 +157,7 @@ private fun TodoItemPreview() {
                 text = "Купить что-то, где-то, зачем-то, но зачем не очень понятно, " +
                         "но точно нужно чтобы показать как обрезается " +
                         "эта часть текста не видна",
-                importance = Importance.BASIC,
+                importance = BASIC,
                 creationDate = date.timeInMillis
             ),
             onCheckboxClick = {},
@@ -129,7 +169,7 @@ private fun TodoItemPreview() {
                 id = "1",
                 isDone = false,
                 text = "Купить что-то",
-                importance = Importance.IMPORTANT,
+                importance = IMPORTANT,
                 creationDate = Calendar.getInstance().timeInMillis,
                 deadline = date.timeInMillis
 
@@ -140,9 +180,20 @@ private fun TodoItemPreview() {
         TodoListItem(
             todoItem = TodoItem(
                 id = "2",
+                isDone = false,
+                text = "Купить что-то",
+                importance = LOW,
+                creationDate = Calendar.getInstance().timeInMillis,
+            ),
+            onCheckboxClick = {},
+            onItemClick = {},
+        )
+        TodoListItem(
+            todoItem = TodoItem(
+                id = "2",
                 isDone = true,
                 text = "Купить что-то",
-                importance = Importance.IMPORTANT,
+                importance = IMPORTANT,
                 creationDate = Calendar.getInstance().timeInMillis,
             ),
             onCheckboxClick = {},
