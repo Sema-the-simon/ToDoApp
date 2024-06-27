16724013bd05eb13cd67440cc78a8ec2df068177
sema <buglaev.sa@edu.spbstu.ru>
2024-06-27 13:21:18 +0300

    delete unused import

--- app/src/main/java/com/example/todoapp/ui/themes/Typography.kt
+++ app/src/main/java/com/example/todoapp/ui/themes/Typography.kt
@@ -1,13 +1,12 @@
 package com.example.todoapp.ui.themes
 
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+
 @Immutable
 data class ExtendedTypography(
     val largeTitle: TextStyle = TextStyle(),
