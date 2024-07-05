eaecd619de7d2321ae922be19e1cbccdb8a95376
sema <buglaev.sa@edu.spbstu.ru>
2024-07-05 16:37:34 +0300

    somefix

--- app/src/main/java/com/example/todoapp/data/network/AuthInterceptor.kt
+++ app/src/main/java/com/example/todoapp/data/network/AuthInterceptor.kt
@@ -4,13 +4,19 @@ import io.ktor.http.HttpHeaders
 import okhttp3.Interceptor
 import okhttp3.Response
 
-private const val TOKEN = "y0_AgAAAAAelQmIAARC0QAAAAEJEQgMAACvplsEl-pH5pna1tIZFabghMyJhQ"
+enum class TokenType {
+    Bearer,
+    OAuth
+}
+
+private const val TOKEN = TODO("Place your token here")
+private val TYPE: String = TokenType.OAuth.name
 
 class AuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val newRequest = originalRequest.newBuilder()
-            .header(HttpHeaders.Authorization, "OAuth $TOKEN").build()
+            .header(HttpHeaders.Authorization, "$TYPE $TOKEN").build()
         return chain.proceed(newRequest)
     }
 }
