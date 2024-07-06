82f650a46795bc96c5fcf88c45a9d9ee1537e9c0
sema <buglaev.sa@edu.spbstu.ru>
2024-07-06 04:40:25 +0300

    clean code

--- app/src/main/java/com/example/todoapp/di/AppModule.kt
+++ app/src/main/java/com/example/todoapp/di/AppModule.kt
@@ -1,9 +1,9 @@
 package com.example.todoapp.di
 
-import com.example.todoapp.data.Repository
 import com.example.todoapp.data.TodoItemsRepository
 import com.example.todoapp.data.network.Api
 import com.example.todoapp.data.network.ApiService
+import com.example.todoapp.domain.Repository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
