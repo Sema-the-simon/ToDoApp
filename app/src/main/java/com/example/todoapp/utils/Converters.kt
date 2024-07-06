fceb287fed7f1c4aeb842c37bf976e56e8668607
sema <buglaev.sa@edu.spbstu.ru>
2024-07-06 06:02:26 +0300

    fix and clear

--- app/src/main/java/com/example/todoapp/utils/Converters.kt
+++ app/src/main/java/com/example/todoapp/utils/Converters.kt
@@ -1,20 +1,12 @@
 package com.example.todoapp.utils
 
-import com.example.todoapp.data.db.entities.TodoItemEntity
 import com.example.todoapp.domain.model.Importance
-import com.example.todoapp.domain.model.TodoItem
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 /**
  * Utility functions and constants.
  */
-
-
-const val IMPORTANCE_IMPORTANT_ID = 3
-const val IMPORTANCE_BASIC_ID = 2
-const val IMPORTANCE_LOW_ID = 1
-
 
 fun formatLongToDatePattern(date: Long): String =
     SimpleDateFormat("d MMM yyyy", Locale.getDefault()).format(date)
@@ -25,19 +17,3 @@ fun String.toImportance(): Importance =
         "basic" -> Importance.BASIC
         else -> Importance.LOW
     }
-fun Importance.getImportanceId(): Int =
-    when (this) {
-        Importance.IMPORTANT -> IMPORTANCE_IMPORTANT_ID
-        Importance.BASIC -> IMPORTANCE_BASIC_ID
-        else -> IMPORTANCE_LOW_ID
-    }
-fun TodoItem.toEntity() =
-    TodoItemEntity(
-        id = this.id,
-        importanceId = this.importance.getImportanceId(),
-        text = this.text,
-        deadline = this.deadline,
-        done = this.isDone,
-        createdAt = this.creationDate,
-        changedAt = this.modificationDate
-    )
