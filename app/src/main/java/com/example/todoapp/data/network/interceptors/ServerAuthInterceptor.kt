de366851230cb49203683172d5c8c2d7b75074cd
sema <buglaev.sa@edu.spbstu.ru>
2024-07-20 15:40:07 +0300

    finally

--- app/src/main/java/com/example/todoapp/data/network/interceptors/ServerAuthInterceptor.kt
+++ app/src/main/java/com/example/todoapp/data/network/interceptors/ServerAuthInterceptor.kt
@@ -1,6 +1,5 @@
 package com.example.todoapp.data.network.interceptors
 
-import android.util.Log
 import com.example.todoapp.BuildConfig
 import io.ktor.http.HttpHeaders
 import okhttp3.Interceptor
@@ -14,7 +13,7 @@ enum class TokenType {
 private val TOKEN: String? =
     if (BuildConfig.DEBUG)
         null // put your token here
-    else System.getenv("API_TOKEN")
+    else BuildConfig.API_TOKEN
 private val TYPE: String = TokenType.OAuth.name
 
 /** Intercepts HTTP requests to add an authorization header with a token. */
@@ -22,10 +21,6 @@ private val TYPE: String = TokenType.OAuth.name
 class ServerAuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
-
-        Log.d("API_TOKEN", "currentBuildConfig is debug :${BuildConfig.DEBUG}")
-        Log.d("API_TOKEN", "currentApiToken is :${BuildConfig.API_TOKEN}")
-
         val token = TOKEN
             ?: throw IllegalArgumentException("Place your token here in TOKEN constant in ServerAuthInterceptor.kt")
         val newRequest = originalRequest.newBuilder()
