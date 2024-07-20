9a3a92945c2a09e1760a9642dc588d924d7acbbf
sema <buglaev.sa@edu.spbstu.ru>
2024-07-20 14:58:40 +0300

    add api token into secret

--- app/src/main/java/com/example/todoapp/data/network/interceptors/ServerAuthInterceptor.kt
+++ app/src/main/java/com/example/todoapp/data/network/interceptors/ServerAuthInterceptor.kt
@@ -1,5 +1,6 @@
 package com.example.todoapp.data.network.interceptors
 
+import android.util.Log
 import com.example.todoapp.BuildConfig
 import io.ktor.http.HttpHeaders
 import okhttp3.Interceptor
@@ -12,7 +13,7 @@ enum class TokenType {
 
 private val TOKEN: String? =
     if (BuildConfig.DEBUG)
-        null // place your token here
+        null // put your token here
     else System.getenv("API_TOKEN")
 private val TYPE: String = TokenType.OAuth.name
 
@@ -21,6 +22,10 @@ private val TYPE: String = TokenType.OAuth.name
 class ServerAuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
+
+        Log.d("API_TOKEN", "currentBuildConfig is debug :${BuildConfig.DEBUG}")
+        Log.d("API_TOKEN", "currentApiToken is :${System.getenv("API_TOKEN")}")
+
         val token = TOKEN
             ?: throw IllegalArgumentException("Place your token here in TOKEN constant in ServerAuthInterceptor.kt")
         val newRequest = originalRequest.newBuilder()
